c77e518f8faaa832fda83f86d2ed594667bebe47
ChenYu <412607705@qq.com>
2019-01-28 10:40:34 +0800

    add three

---
+++ 认识Pyrhon/02_分支/hm_04_判断考试成绩.py
@@ -0,0 +1,8 @@
+# 练习2：定义两个整数变零python_score,c_score,编写代码判断成绩
+python_score = 80
+c_score = 60
+# # 要求只要有一门成绩>60就算合格
+if python_score >60 or c_score > 60:
+    print("成绩合格")
+else:
+    print("考试失败，继续努力！")
